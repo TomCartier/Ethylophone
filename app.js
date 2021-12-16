8d41f1bbe27205aae7c217ef297785e8a338f4b5
TomCartier <95412169+TomCartier@users.noreply.github.com>
2021-12-16 15:21:44 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var slide = new Array("/Images/proto_apple (1).png", "/Images/proto_universal (1).png");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
